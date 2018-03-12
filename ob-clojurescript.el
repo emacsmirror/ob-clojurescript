a2ddbea73de1b4153675f10e23d24753c1f5f69c
Larry Staton Jr <larry@larrystaton.com>
2018-03-12 03:51:56 +0000

    Update README
    Add license and package-requires

--- ob-clojurescript.el
+++ ob-clojurescript.el
@@ -5,6 +5,9 @@
 ;; Created: 10 March 2018
 ;; Keywords: literate programming, reproducible research
 ;; Homepage: https://gitlab.com/statonjr/ob-clojurescript
+;; Package-Requires: ((emacs "24.4") (org "9.0"))
+
+;; This file is not part of GNU Emacs.
 
 ;;; Commentary:
 
@@ -14,6 +17,23 @@
 
 ;; - [[https://github.com/anmonteiro/lumo][lumo]]
 ;; - clojurescript-mode
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 ;;; Code:
 (require 'ob)
